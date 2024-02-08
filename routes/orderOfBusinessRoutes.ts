27db43c59d60b53185c628857d83bedc07808974
Alex Simisi Nta <smsxander@gmail.com>
2024-02-08 19:53:44 +0100

    update file

--- routes/orderOfBusinessRoutes.ts
+++ routes/orderOfBusinessRoutes.ts
@@ -3,10 +3,10 @@ import OrderOfBusinessController from "../controllers/OrderOfBusinessController"
 
 const router: Router = Router();
 
-router.post("/", OrderOfBusinessController.create);
-router.get("/", OrderOfBusinessController.getAll);
-router.get("/:id", OrderOfBusinessController.getById);
-router.put("/:id", OrderOfBusinessController.update);
-router.delete("/:id", OrderOfBusinessController.delete);
+router.post("/order-of-business", OrderOfBusinessController.create);
+router.get("/order-of-business", OrderOfBusinessController.getAll);
+router.get("/order-of-business/:id", OrderOfBusinessController.getById);
+router.put("/order-of-business/:id", OrderOfBusinessController.update);
+router.delete("/order-of-business/:id", OrderOfBusinessController.delete);
 
 export default router;
