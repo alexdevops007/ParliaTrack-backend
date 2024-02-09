d0450e6ae66edef67948b6a96f434655199e1fbe
Alex Simisi Nta <smsxander@gmail.com>
2024-02-09 01:54:32 +0100

    update debate routes

--- routes/debateRoutes.ts
+++ routes/debateRoutes.ts
@@ -3,11 +3,11 @@ import DebateController from "../controllers/DebateController";
 
 const router: Router = Router();
 
-router.post("/", DebateController.create);
-router.get("/", DebateController.getAll);
-router.get("/:id", DebateController.getById);
-router.put("/:id/add-note", DebateController.addNote);
-router.put("/:id/mark-key-moment", DebateController.markKeyMoment);
-router.delete("/:id", DebateController.delete);
+router.post("/debates", DebateController.create);
+router.get("/debates", DebateController.getAll);
+router.get("/debates/:id", DebateController.getById);
+router.put("/debates/:id/add-note", DebateController.addNote);
+router.put("/debates/:id/mark-key-moment", DebateController.markKeyMoment);
+router.delete("/debates/:id", DebateController.delete);
 
 export default router;
